a915c9dfed29f0204dd3952e365e862bef34c60a
Yonty <jonathanpo@mta.ac.il>
2024-06-06 19:10:12 +0300

    init

---
+++ B24-Ex02-BarBichachi-315852111-JonathanPoran-209012186/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Ex02.ConsoleUtils;
+
+namespace B24Ex02BarBichachi315852111JonathanPoran209012186
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
